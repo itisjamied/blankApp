fdeb55bd0174ad7da3f382fe71fc4767e9aae17b
HandyDorceus <77027304+itisjamied@users.noreply.github.com>
2023-07-13 12:19:33 -0400

    added react imports

--- blank_app/app/(tabs)/index.tsx
+++ blank_app/app/(tabs)/index.tsx
@@ -1,8 +1,8 @@
 import { StyleSheet } from 'react-native';
-
+import React from 'react';
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
-import React from 'react';
+
 
 export default function TabOneScreen() {
   return (
